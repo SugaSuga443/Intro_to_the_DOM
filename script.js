8a578b26bfba39310a5cf527d6059bf19e9c75b0
sugasuga443 <quetta.lonon@gmail.com>
2025-04-06 17:28:08 -0400

    intial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const title = document.getElementById("title");
+console.log(title);
+
+const pTag = document.querySelector("p");
+const buttonTag = document.querySelector("button");
+//console.log(pTag, buttonTag);
+console.log(pTag);
+console.log(buttonTag);
+
+const psTag = document.querySelector("p.p2").innerText;
+console.log(psTag);
+
+
+const myText = document.querySelector("p");
+myText.style.fontSize = "30px";
+
+const header = document.querySelectorAll("h2");
+console.log(header);
